0565c01d5318d3bebf3f7803d1a98c77e7609dd7
Shubham Pandey <shubhampandey979271@gmail.com>
2021-11-09 17:09:02 +0530

    Finally Done

--- InstagramStory/src/main/java/papayacoders/instastory/Stories.java
+++ InstagramStory/src/main/java/papayacoders/instastory/Stories.java
@@ -36,7 +36,6 @@ public class Stories {
             getStoriesApi(context);
         } else {
             Toast.makeText(context, "Please login first.", Toast.LENGTH_SHORT).show();
-//            initLoginDialog(context);
         }
     }
 
@@ -45,53 +44,6 @@ public class Stories {
 
     }
 
-//    private static void initLoginDialog(Context context) {
-//        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context, R.style.SheetDialog);
-//        bottomSheetDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        bottomSheetDialog.setContentView(R.layout.dialog_login);
-//        bottomSheetDialog.show();
-//        TextView textViewYes = bottomSheetDialog.findViewById(R.id.textViewYes);
-//        textViewYes.setOnClickListener(new View.OnClickListener() {
-//            public final void onClick(View view) {
-//
-//                bottomSheetDialog.dismiss();
-//
-//                if (!SharePrefs.getInstance(context).getBoolean(SharePrefs.IS_INSTAGRAM_LOGIN)) {
-//
-//                    return;
-//                }
-//
-//            }
-//        });
-//
-//        TextView textViewCancel = bottomSheetDialog.findViewById(R.id.textViewCancel);
-//        textViewCancel.setOnClickListener(view -> bottomSheetDialog.dismiss());
-//    }
-
-
-    //    @Override
-//    public void onActivityResult(int i, int i2, Intent intent) {
-//        try {
-//            super.onActivityResult(i, i2, intent);
-//            if (i != 100 || i2 != -1) {
-//                return;
-//            }
-//            if (SharePrefs.getInstance(this).getBoolean(SharePrefs.IS_INSTAGRAM_LOGIN)) {
-////                this.textViewLogin.setText(getResources().getString(R.string.logout));
-////                this.linearLayoutStories.setVisibility(View.VISIBLE);
-////                this.linearLayoutPlaceHolder.setVisibility(View.GONE);
-//                Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
-//                getStoriesApi(this);
-//                return;
-//            }
-//            Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
-////            this.textViewLogin.setText(getResources().getString(R.string.login));
-////            this.linearLayoutStories.setVisibility(View.GONE);
-////            this.linearLayoutPlaceHolder.setVisibility(View.VISIBLE);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
     public static boolean isNetworkAvailable(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
@@ -106,7 +58,6 @@ public class Stories {
             if (!isNetworkAvailable(context)) {
                 Toast.makeText(context, context.getResources().getString(R.string.no_net_conn), Toast.LENGTH_SHORT).show();
             } else if (commonAPI != null) {
-//                progressLoadingBar.setVisibility(View.VISIBLE);
                 CommonAPI commonAPI1 = commonAPI;
                 DisposableObserver<FullDetailModel> disposableObserver = storyDetailObserver;
                 commonAPI1.getFullFeed(disposableObserver, str, "ds_user_id=" + SharePrefs.getInstance(context).getString(SharePrefs.USERID) + "; sessionid=" +
@@ -119,14 +70,8 @@ public class Stories {
 
     private static DisposableObserver<FullDetailModel> storyDetailObserver = new DisposableObserver<FullDetailModel>() {
         public void onNext(FullDetailModel fullDetailModel) {
-//            recyclerViewUser.setVisibility(View.VISIBLE);
-//            progressLoadingBar.setVisibility(View.GONE);
             try {
                 mStoryList.postValue(fullDetailModel.getReelFeed().getItems());
-
-//                storyAdapter = new StoryAdapter(MainActivity.this, fullDetailModel.getReelFeed().getItems());
-//                recyclerViewStories.setAdapter(storyAdapter);
-//                storyAdapter.notifyDataSetChanged();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -134,13 +79,11 @@ public class Stories {
 
         @Override
         public void onError(Throwable th) {
-//            progressLoadingBar.setVisibility(View.GONE);
             th.printStackTrace();
         }
 
         @Override
         public void onComplete() {
-//            progressLoadingBar.setVisibility(View.GONE);
         }
     };
 
@@ -160,7 +103,6 @@ public class Stories {
             if (!isNetworkAvailable(context)) {
                 Toast.makeText(context, "No Internet", Toast.LENGTH_SHORT).show();
             } else if (commonAPI != null) {
-//                progressLoadingBar.setVisibility(View.VISIBLE);
                 CommonAPI commonAPI1 = commonAPI;
                 DisposableObserver<StoryModel> disposableObserver = storyObserver;
                 commonAPI1.getStories(disposableObserver, "ds_user_id=" +
@@ -175,9 +117,6 @@ public class Stories {
     private static DisposableObserver<StoryModel> storyObserver = new DisposableObserver<StoryModel>() {
 
         public void onNext(StoryModel storyModel) {
-//            recyclerViewUser.setVisibility(View.VISIBLE);
-//            progressLoadingBar.setVisibility(View.GONE);
-
             Log.d("StoryModal", "onNext: " + storyModel);
             try {
                 ArrayList<TrayModel> arrayList = new ArrayList<>();
@@ -195,9 +134,6 @@ public class Stories {
                     dialog.dismiss();
 
                 Log.d("MISTER", "onNext: " + arrayList);
-
-//                profileAdapter = new ProfileAdapter(MainActivity.this, arrayList, MainActivity.this);
-//                recyclerViewUser.setAdapter(profileAdapter);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -205,14 +141,11 @@ public class Stories {
 
         @Override
         public void onError(Throwable th) {
-//            progressLoadingBar.setVisibility(View.GONE);
             th.printStackTrace();
         }
 
         @Override
         public void onComplete() {
-//            progressLoadingBar.setVisibility(View.GONE);
-
         }
     };
 
@@ -229,14 +162,5 @@ public class Stories {
         SharePrefs.getInstance(context).putString(SharePrefs.SESSIONID, "");
         SharePrefs.getInstance(context).putString(SharePrefs.USERID, "");
         mList.postValue(null);
-//        if (SharePrefs.getInstance(context).getBoolean(SharePrefs.IS_INSTAGRAM_LOGIN).booleanValue()) {
-//            textViewLogin.setText(getResources().getString(R.string.logout));
-//            linearLayoutStories.setVisibility(View.VISIBLE);
-//            linearLayoutPlaceHolder.setVisibility(View.GONE);
-//        } else {
-//            textViewLogin.setText(getResources().getString(R.string.login));
-//            linearLayoutStories.setVisibility(View.GONE);
-//            linearLayoutPlaceHolder.setVisibility(View.VISIBLE);
-//        }
     }
 }
